663c79e227e7ce35ff8e6a58b33c80e81e599250
Austin Platt <austinplatt@gmail.com>
2014-03-28 01:42:21 +0000

    working??

--- project.clj
+++ project.clj
@@ -6,4 +6,5 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [enlive "1.1.5"]
                  [org.clojure/math.combinatorics "0.0.7"]
-                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]])
+                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
+                 [aysylu/loom "0.4.2"]])
--- src/rap_graph/core.clj
+++ src/rap_graph/core.clj
@@ -1,45 +1,65 @@
 (ns rap-graph.core
-  (:require [rap-graph.graph :as g]
+  (:require ;[rap-graph.graph :as g]
+            [loom.graph :as g]
+            [loom.alg :as ga]
+            [loom.attr :as graph-attr]
             [rap-graph.scrape :as scrape]
             [clojure.math.combinatorics :as combo]
             [clojure.core.async :refer [go chan timeout >! alts!! close!]]))
 
-(defn add-scrape-to-graph [graph artists]
-  (let [artist-combinations (combo/combinations artists 2)]
-    (reduce (fn [graph [artist-1 artist-2]]
-              (g/add-edge graph artist-1 artist-2))
-            graph
-            artist-combinations)))
+(defn add-scrape-to-graph [graph artists song]
+  (let [artist-combinations (combo/combinations artists 2)
+        new-graph (reduce (fn [graph [artist-1 artist-2]]
+                  (g/add-edges graph [artist-1 artist-2]))
+                graph
+                artist-combinations)]
+    (graph-attr/add-attr-to-edges new-graph :song song artist-combinations)))
 
 (defn crawl [seed-artist depth ch]
   (def artists-seen (atom #{}))
   (letfn [(crawl-artist [artist depth ch]
             (println artist)
             (swap! artists-seen conj artist)
-            (go (let [songs (scrape/find-songs artist)]
-                  (doseq [song songs]
-                    (crawl-song song depth ch)))))
+            (let [songs (scrape/find-songs artist)]
+                (doseq [song songs]
+                  (crawl-song song depth ch))))
           (crawl-song [url depth ch]
-            (go (let [artists (scrape/find-artists url)]
-                  (>! ch artists)
+            (let [artists (scrape/find-artists url)]
+              (go (>! ch  {:artists artists :url url})
                   (doseq [artist artists]
                     (when (and (pos? depth) (not (contains? @artists-seen artist)))
-                      (crawl-artist artist (dec depth) ch)))
-                  )))]
+                      (crawl-artist artist (dec depth) ch))))
+                   ))]
     (crawl-artist seed-artist depth ch)))
 
+(defn mock-crawl [ch]
+  (dotimes [i 10]
+    (println i)
+    (go (>! ch {:artists [(str "a" i) (str "a" (* i 2)) (str "a" (* i 3))]
+                :url (str "url" i)}))))
+
 (defn make-graph [seed-artist depth]
-  (def g (atom {}))
+  (def rg (atom (g/graph)))
   (let [ch (chan)]
     (crawl seed-artist depth ch)
+    ;(mock-crawl ch)
     (loop []
-      (let [tmo (timeout 10000)
-            [msg chan] (alts!! [tmo ch])]
-        (if (= chan tmo)
-          (do (close! ch)
-              (println "Finished"))
-          (do
-            (swap! g add-scrape-to-graph msg)
-            ;(println g)
-            (recur)))))
-    @g))
+        (let [tmo (timeout 10000)
+              [msg chan] (alts!! [tmo ch])]
+          (if (= chan tmo)
+            (do (close! ch)
+                (println "Finished"))
+            (do
+              (print msg)
+              (swap! rg add-scrape-to-graph (:artists msg) (:url msg))
+                                        ;(println g)
+              (recur)))))
+    @rg))
+
+
+(defn artist-path [graph start finish]
+  (let [path (ga/bf-path graph start finish)]
+    (map (fn [[artist1 artist2]]
+           {:collab (str artist1 " -> " artist2)
+            :song (:song (graph-attr/attrs graph artist1 artist2))})
+         (partition 2 1 path))))
